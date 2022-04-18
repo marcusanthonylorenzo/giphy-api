6d750faa1d70c8f71b35ab94946b5518ed3d27a6
Marcus <marcanthonyzpadilla@gmail.com>
2022-04-18 14:36:42 -0700

    set initial commit
    Co-authored-by: jfox25 <jfox25@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
+import $ from 'jquery';
 import "./css/styles.css";
-import { } from "./../src/js/objects.js";
+
+//process.env.API_KEY
+$("#search").click(function () {
+
+  let request = new XMLHttpRequest();
+  let url = `api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=cheeseburgers`;
+
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+    }
+  };
+
+  request.open("GET", url, true);
+
+  request.send();
+});
+
